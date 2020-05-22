3b440b80b7afa07c869b12451d3dd76f3cab1162
Prachi <prachi.jain@bacancytechnology.com>
2020-05-22 12:15:37 +0530

    single author and book query

--- server.js
+++ server.js
@@ -44,15 +44,51 @@ const BookType = new GraphQLObjectType({
     })
 })
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents an author of a book',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books: { 
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id)
+            }
+        }
+    })
+})
+
 const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A single Book',
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id)
+            },
         books: {
         type: new GraphQLList(BookType),
         description: 'List of All Books',
         resolve: () => books 
-        }
+        },
+        authors: {
+        type: new GraphQLList(AuthorType),
+        description: 'List of All Authors',
+        resolve: () => authors 
+        },
+        author: {
+            type: AuthorType,
+            description: 'A signle author',
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id)
+            }
     })
 })
 
